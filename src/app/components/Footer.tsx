1ebc68d6820bf4c1615634c907d8c5ff9efcaa21
Clariana Abreu <145071003+abreuclariana@users.noreply.github.com>
2026-03-26 10:16:28 -0300

    feat: update profile with full name and professional branding

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
     const currentYear = new Date().getFullYear();
     return (
       <footer className="py-8 px-4 text-center bg-zinc-900 text-gray-300 text-base dark:text-gray-300 light:text-gray-600 ">
-        © {currentYear} Clariana Silva de Abreu. All Rights Reserved
+        © {currentYear} Clariana Abreu. All Rights Reserved
       </footer>
     );
   }
